e88f74b255493c26050bcd296bfdc458331f68ba
SabinaGarayeva <sgarayeva13@gmail.com>
2024-08-11 22:01:29 +0100

    Context API for state management

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -3,6 +3,7 @@ import { Spinnaker } from "next/font/google";
 import { Suspense } from "react";
 import Spinner from "../_components/Spinner";
 import Filter from "../_components/Filter";
+import ReservationReminder from "../_components/ReservationReminder";
 
 export const revalidate = 3600;
 
@@ -31,6 +32,7 @@ export default async function Page({ searchParams }) {
       </div>
       <Suspense fallback={<Spinner />} key={filter}>
         <CabinList filter={filter} />
+        <ReservationReminder/>
       </Suspense>
     </div>
   );
